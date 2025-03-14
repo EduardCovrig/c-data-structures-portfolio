0c234d669366cae5ea8478681f2074ee7017543d
eduardcovrig <eduardcovrig@EDI4913>
2025-03-14 09:45:59 +0200

    aa

--- 1056_Covrig_Eduard_Gabriel_tema1.c
+++ 1056_Covrig_Eduard_Gabriel_tema1.c
@@ -1,3 +1,22 @@
 #include <iostream>
+#include <fstream>
 
-dadad
+struct Ecran
+{
+	int latime;
+	int inaltime;
+	char* rezolutieMaxima;
+	char* tipPanou;
+	float pret;
+	int refreshRate;
+	char* brand;
+	bool suportHDR;
+};
+
+int main()
+{
+	FILE* f = fopen("ecrane.txt",r);
+
+
+	return 0;
+}
