1fbb587fc9ac30e24b70bf4e3fbc4790c0ffdb18
EduardCovrig <eduuardcovrig@gmail.com>
2025-03-15 15:45:53 +0200

    Add files via upload

---
+++ 1056_Covrig_Eduard_Gabriel_tema1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <fstream>
+
+struct Ecran
+{
+	float latime;
+	float inaltime;
+	char* tipPanou;
+	char* rezolutieMaxima;
+	int refreshRate;
+	bool areHDR;
+	char* brand;
+	float pret;
+
+};
+
+void citireEcrane
+
+int main()
+{
+	FILE* f = fopen("ecrane.txt", "r");
+
+
+	return 0;
+}
